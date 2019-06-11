c41d1704e7d450f1b2374ab6695ce248228245d7
yangrui <happy2019>
2019-06-11 14:59:00 +0800

    生日

--- sm-service/src/main/java/com/sm/service/function/BirthdayThread.java
+++ sm-service/src/main/java/com/sm/service/function/BirthdayThread.java
@@ -1,15 +1,11 @@
 package com.sm.service.function;
 
-import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSelect;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.sm.business.model.Birth;
 import com.sm.business.service.BirthService;
 import org.iframework.support.spring.context.BaseSpringContextSupport;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.util.CollectionUtils;
 
@@ -28,19 +24,8 @@ public class BirthdayThread implements Runnable {
         try {
             BirthService birthService = (BirthService) BaseSpringContextSupport.getApplicationContext().getBean("birthService");
             System.out.println("====task " + month + "开始执行");
-            // 得到浏览器对象，直接New一个就能得到，现在就好比说你得到了一个浏览器了
-            WebClient webClient = new WebClient();
-            // 下面这2句可以写，也可以不写，设置false就是不加载css和js。访问速度更快
-            webClient.getOptions().setCssEnabled(false);
-            webClient.getOptions().setJavaScriptEnabled(false);
-            webClient.getOptions().setThrowExceptionOnScriptError(false);
-            // 当出现Http error时，程序不抛异常继续执行
-            webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
-            webClient.getOptions().setUseInsecureSSL(false);
-            // 做的第一件事，去拿到这个网页，只需要调用getPage这个方法即可
-            HtmlPage page = webClient.getPage("https://www.buyiju.com/birth/");
-            // 根据名字得到一个表单，查看上面这个网页的源代码可以发现表单的名字叫“f”
-            HtmlForm form = page.getFormByName("birth");
+
+            HtmlForm form = WebClientUtil.getForm("https://www.buyiju.com/birth/", "birth");
             HtmlSubmitInput button = form.getInputByValue("开始测试");
 
             HtmlSelect sx1 = form.getSelectByName("month");
@@ -50,11 +35,7 @@ public class BirthdayThread implements Runnable {
                 HtmlSelect sx2 = form.getSelectByName("day");
                 sx2.setSelectedIndex(i);
 
-                HtmlPage nextPage = button.click();
-                // 把获得后的网页转换成document
-                Document document = Jsoup.parse(nextPage.asXml());
-                // 获得配对文本结果
-                Element tagElement = document.getElementsByClass("content").first();
+                Element tagElement = WebClientUtil.getElement(button);
                 if (tagElement != null) {
                     tagElement.select("div.yunshi").remove();
                     tagElement.select("a").remove();
---
+++ sm-service/src/main/java/com/sm/service/function/PhoneUtil.java
@@ -0,0 +1,42 @@
+package com.sm.service.function;
+
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+import org.jsoup.nodes.Element;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PhoneUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(PhoneUtil.class);
+    public static Map<String, Object> getResult(String phone) {
+
+        Map<String, Object> result = new HashMap<>();
+        try {
+            HtmlForm form = WebClientUtil.getForm("https://www.buyiju.com/shouji/", "buyiju");
+            HtmlSubmitInput button = form.getInputByValue("开始测试");
+
+            HtmlTextInput input = form.getInputByName("sjhao");
+            input.setValueAttribute(phone);
+
+            Element tagElement = WebClientUtil.getElement(button);
+            if (tagElement != null) {
+                tagElement.select("div.inform_vip").remove();
+                tagElement.getElementsByTag("p").get(7).remove();
+
+                String content = tagElement.toString()
+                        .replaceAll("<div class=\"content\">","")
+                        .replaceAll("</div>","");
+                result.put("data",content);
+                logger.info("=======================" + content);
+            }
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+}
