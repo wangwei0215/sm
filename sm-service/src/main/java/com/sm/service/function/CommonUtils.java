262f2d6f7a495997d4288f84116c8b8ba73d769f
liguangcun <liguangcun@chengmish.com>
2019-06-11 14:32:39 +0800

    爬虫运程

---
+++ sm-service/src/main/java/com/sm/service/function/CommonUtils.java
@@ -0,0 +1,19 @@
+package com.sm.service.function;
+
+import net.sf.json.JSONObject;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+/**
+ * Created by liguangcun on 2019/6/11.
+ */
+public class CommonUtils {
+    public static void print(HttpServletResponse response, Map<String,String> map) throws Exception{
+        JSONObject json = JSONObject.fromObject(map);
+        String result = "showData" + "(" + json.toString() + ")";
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().write(result);
+    }
+}
